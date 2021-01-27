b6a93fa08bf79aa37d9a420852506f798cefa0cb
Benoit Bovy <benbovy@gmail.com>
2021-01-27 17:34:24 +0100

    add first workshop notebooks

---
+++ notebooks/doc.md
@@ -0,0 +1,27 @@
+# Online ressources and documentation
+
+## Fastscape
+
+- Fastscape website: https://fastscape.org/
+
+
+- Fastscape repositories on GitHub: https://github.com/fastscape-lem
+- Discussion forum: https://discourse.fastscape.org/
+- Chat: https://gitter.im/fastscape-lem/lobby
+- Twitter: https://twitter.com/fastscape
+
+
+- Fastscape (python high-level interface) documentation: https://fastscape.readthedocs.io
+- Fastscapelib-fortran documentation: https://fastscape.org/fastscapelib-fortran/
+
+## Xarray (+ extensions)
+
+- Xarray documentation: http://xarray.pydata.org
+- Xarray-simlab documentation: https://xarray-simlab.readthedocs.io
+- Xarray-spatial documentation: https://makepath.github.io/xarray-spatial/ 
+
+## Visualization
+
+- Xarray plotting documentation: http://xarray.pydata.org/en/stable/plotting.html
+- Hvplot documentation: https://hvplot.holoviz.org/
+- Ipyfastscape documentation: not yet available
---
+++ notebooks/xr_utils.py
@@ -0,0 +1,40 @@
+
+
+def map_xr_func(func, da, time_dim=None, batch_dim=None, **kwargs):
+    """Map a DataArray compatible function, maybe brodcasting it over
+    the time and/or batch dimension(s).
+    
+    Parameters
+    ----------
+    func : callable
+        Any function accepting a :class:`xarray.DataArray` as first and
+        only positional argument and returning another DataArray.
+    da : :class:`xarray.DataArray`
+        The DataArray object used to map ``func``.
+    time_dim : str, optional
+        Time dimension label.
+    batch_dim : str
+        Batch dimension label.
+    **kwargs
+        Keyword arguments passed to ``func``.
+        
+    Returns
+    -------
+    result : :class:`xarray.DataArray`
+        The resulting DataArray.
+
+    """
+    if batch_dim is not None:
+        res_t = []
+        for _, da_t in da.groupby(batch_dim):
+            res_t.append(map_xr_func(func, da, time_dim=time_dim, **kwargs))
+        
+        result = xr.concat(res_t, batch_dim)
+
+    elif time_dim is not None:
+        result = elev.groupby(groupby).map(func, shortcut=False, **kwargs)
+
+    else:
+        result = func(da, **kwargs)
+    
+    return result
